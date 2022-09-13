4062b2ae83f2df92ee1a5801a97a5cc2cba835fd
soliu yusuf <soliuy5@gmail.com>
2022-09-13 12:00:33 +0100

    h

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 
 import React from 'react'
 
-import background from "../assets/images/pattern-bg.png";
-import arrow from '../assets/images/icon-arrow.svg';
+import background from "../assets/images/pattern-bg.png"
 
 
 const Header = ({ipData, fetchMoviesHandler, inputText, InputHandler, SubmitHandler, enteredNameIsInValid, nameInputBlurHandler, error}) => {
@@ -23,10 +22,10 @@ const Header = ({ipData, fetchMoviesHandler, inputText, InputHandler, SubmitHand
             <div className='flex justify-center flex-col items-center '>
                 <h1 className='text-2xl text-white my-5'>IP Address Tracker</h1>
 
-                <form action="" onClick={SubmitHandler} className="p-3" >
+                <form action="" onClick={SubmitHandler} >
                     <input type="text" size="50" placeholder='search for any IP or domain' onBlur={nameInputBlurHandler} onChange={InputHandler} value={inputText} className='p-3 outline-none border rounded-l-2xl ' />
                     {enteredNameIsInValid && <p>IP cannot be empty</p>}
-                    <button className='bg-black p-5 rounded-r-2xl text-white'><img src={arrow} alt="" /></button>
+                    <button className='bg-black p-3 rounded-r-2xl text-white'>search</button>
                     {error && <p>Put correct address</p>}
                 </form>
              {/* <IpDetails ipData={ipData}/> */}
